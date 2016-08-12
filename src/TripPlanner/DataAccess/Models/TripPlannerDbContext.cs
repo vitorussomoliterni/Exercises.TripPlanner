472db04f35b08ebdb9d4e59a83e7ba0929946ece
Rondon Correa <Juan.Rondoncorrea1@DET>
2016-08-12 11:55:54 +1000

    Created Entities and Customer Service

---
+++ src/TripPlanner/DataAccess/Models/TripPlannerDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TripPlanner.DataAccess.Models
+{
+    public class TripPlannerDbContext : DbContext
+    {
+        public DbSet<Customer> Customers { get; set; }
+
+        public TripPlannerDbContext(DbContextOptions<TripPlannerDbContext> options) : base(options) { }
+    }
+}
---
+++ src/TripPlanner/Migrations/20160812012212_InitialCreate.Designer.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using TripPlanner.DataAccess.Models;
+
+namespace TripPlanner.Migrations
+{
+    [DbContext(typeof(TripPlannerDbContext))]
+    [Migration("20160812012212_InitialCreate")]
+    partial class InitialCreate
+    {
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.0.0-rtm-21431")
+                .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+            modelBuilder.Entity("TripPlanner.DataAccess.Models.Customer", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<DateTime>("DateOfBirth");
+
+                    b.Property<string>("FirstName")
+                        .IsRequired();
+
+                    b.Property<string>("LastName")
+                        .IsRequired();
+
+                    b.Property<string>("Postcode")
+                        .IsRequired()
+                        .HasColumnType("char(4)");
+
+                    b.Property<string>("State")
+                        .IsRequired()
+                        .HasColumnType("char(3)");
+
+                    b.Property<string>("StreetAddress")
+                        .IsRequired();
+
+                    b.Property<string>("Suburb")
+                        .IsRequired();
+
+                    b.HasKey("Id");
+
+                    b.ToTable("Customers");
+                });
+        }
+    }
+}
