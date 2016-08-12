f93c720166f5c88477693fd2bffbe2c50cec18db
Vito Russo Moliterni <vito.russo.moliterni@gmail.com>
2016-08-12 11:00:14 +1000

    Added customer entity

--- src/TripPlanner/DataAccess/Models/Customer.cs
+++ src/TripPlanner/DataAccess/Models/Customer.cs
@@ -1,6 +1,27 @@
-﻿namespace TripPlanner.DataAccess.Models
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TripPlanner.DataAccess.Models
 {
     public class Customer
     {
+        public int Id { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        public DateTime DateOfBirth { get; set; }
+        [Required]
+        public string StreetAddress { get; set; }
+        [Required]
+        public string Suburb { get; set; }
+        [Required]
+        [Column(TypeName = "varchar(3)")]
+        public string State { get; set; }
+        [Required]
+        [Column(TypeName = "varchar(4)")]
+        public string Postcode { get; set; }
     }
 }
