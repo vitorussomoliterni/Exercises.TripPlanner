b2ab912ac12682573b85b43d7bff9cdca5fab248
Vito Russo Moliterni <vito.russo.moliterni@gmail.com>
2016-08-12 11:11:52 +1000

    Added db context class

--- src/TripPlanner/DataAccess/Models/TripPlannerDbContext.cs
+++ src/TripPlanner/DataAccess/Models/TripPlannerDbContext.cs
@@ -1,11 +1,19 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace TripPlanner.DataAccess.Models
 {
-    public class TripPlannerDbContext
+    public class TripPlannerDbContext : DbContext
     {
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Package> Packages { get; set; }
+        public DbSet<PackageTrip> PackageTrips { get; set; }
+        public DbSet<TransportType> TransportTypes { get; set; }
+        public DbSet<Trip> Trips { get; set; }
+
+
     }
 }
