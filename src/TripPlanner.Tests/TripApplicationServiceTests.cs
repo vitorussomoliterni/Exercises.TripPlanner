b1eaa7cb386e4c34330dad096a7ae60e90387c32
Vito Russo Moliterni <vito.russo.moliterni@gmail.com>
2016-08-14 16:33:05 +1000

    Added service test class

---
+++ src/TripPlanner.Tests/TripApplicationServiceTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Xunit;
+
+namespace TripPlanner.Tests
+{
+    public class TripApplicationServiceTests
+    {
+        [Fact]
+        public async void CreateServiceTest()
+        {
+            // Set up fixture
+            var optionsBuilder = new DbContextOptionsBuilder<TripPlannerDbContext>();
+        }
+    }
+}
