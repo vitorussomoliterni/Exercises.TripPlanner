13dd5621f90d8c8a66119cb17d300378829b1bf2
Vito Russo Moliterni <vito.russo.moliterni@gmail.com>
2016-08-14 12:35:24 +1000

    Added service methods for trip service

--- src/TripPlanner/Services/TripApplicationService.cs
+++ src/TripPlanner/Services/TripApplicationService.cs
@@ -2,10 +2,32 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TripPlanner.DataAccess.Models;
 
 namespace TripPlanner.Services
 {
     public class TripApplicationService
     {
+        TripPlannerDbContext _context;
+
+        public TripApplicationService(TripPlannerDbContext context)
+        {
+            _context = context;
+        }
+
+        public Trip CreateTrip(DateTime tripDate, string origin, string destination, int transportTypeId)
+        {
+            return null;
+        }
+
+        public Trip UpdateTrip(int tripId, DateTime tripDate, string origin, string destination, int transportTypeId)
+        {
+            return null;
+        }
+
+        public void DeleteTrip(int tripId)
+        {
+
+        }
     }
 }
